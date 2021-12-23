c367cedb4bff756f6d3b9aaf77c8fa7dad59a175
ChibiKev <k3v.ch3n@gmail.com>
2021-12-22 20:05:19 -0500

    Added Fifth Problem

---
+++ 27. Remove Element/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+  int removeElement(vector<int>& nums, int val) {
+    int size = nums.size();
+    int i = 0;
+    while(i < size){
+      if(nums[i] == val){
+        nums.erase(nums.begin() + i);
+        size--;
+        continue;
+      }
+      i++;
+    }
+    return size;
+  }
+};
