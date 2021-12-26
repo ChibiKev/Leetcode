e6ad2ddb0c54c6f5fee7a652cf8e726a5f036ce8
ChibiKev <k3v.ch3n@gmail.com>
2021-12-26 16:08:37 -0500

    Added Javascript Solution for Tenth Problem

---
+++ 2108. Find First Palindromic String in the Array/solution.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string[]} words
+ * @return {string}
+ */
+ var firstPalindrome = function(words) {
+  let result = "";
+  for (let word of words){
+    let lengthOfWord = word.length;
+    let found = false;
+    for(let i = 0; i < lengthOfWord/2; i++){
+      if(word[i] === word[lengthOfWord - i - 1]){
+        found = true;
+      }
+      else{
+        found = false;
+        break;
+      }
+    }
+    if(found === true){
+      result = word;
+      break;
+    }
+  }
+  return result;
+};
