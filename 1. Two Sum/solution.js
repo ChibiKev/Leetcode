8dfcad7406f0866e5420e16f28e36b516611577a
ChibiKev <k3v.ch3n@gmail.com>
2022-01-07 15:49:41 -0500

    Minor Changes

--- 1. Two Sum/solution.js
+++ 1. Two Sum/solution.js
@@ -4,7 +4,7 @@
  * @return {number[]}
  */
  var twoSum = function(nums, target) {
-  let dictionary = {}
+  let dictionary = {};
   for (let index = 0; index < nums.length; index++){
     let value = nums[index];
     let search = target - value;
