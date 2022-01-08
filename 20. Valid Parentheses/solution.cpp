118a751f540c2f6b502a707ed8654a7fc863376b
ChibiKev <k3v.ch3n@gmail.com>
2022-01-08 00:57:56 -0500

    Modified C++ Solution To Third Problem

--- 20. Valid Parentheses/solution.cpp
+++ 20. Valid Parentheses/solution.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
   bool isValid(string s) {
-    vector<char> storage;
+    vector<char> stack;
+
     for(int i = 0; i < s.length(); i++){
-      if(s[i] == '{'){
-        storage.push_back('}');
+      char bracket = s[i];
+      if(bracket == '(' || bracket == '['|| bracket == '{'){
+        stack.push_back(bracket);
       }
-      else if(s[i] == '('){
-        storage.push_back(')');
+      else if(stack.size() == 0){
+        return false;
       }
-      else if(s[i] == '['){
-        storage.push_back(']');
+      else if(bracket == ')' && stack[stack.size() - 1] == '('){
+        stack.pop_back();
+      }
+      else if(bracket == ']' && stack[stack.size() - 1] == '['){
+        stack.pop_back();
+      }
+      else if(bracket == '}' && stack[stack.size() - 1] == '{'){
+        stack.pop_back();
       }
       else{
-        if(storage.size() == 0) return false;
-        if(s[i] == storage[storage.size() - 1]){
-          storage.pop_back();
-        }
-        else{
-          return false;
-        }
+        return false;
       }
     }
-    if(storage.size() == 0){
-      return true;
-    }
-    return false;
+
+    return stack.size() == 0;
   }
 };
