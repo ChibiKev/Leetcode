c1f875bc40b664377609ba7281bf90b53502b9fa
ChibiKev <k3v.ch3n@gmail.com>
2022-01-14 19:06:18 -0500

    Added Thirty Eighth Problem

---
+++ 1450. Number of Students Doing Homework at a Given Time/solution.py
@@ -0,0 +1,14 @@
+class Solution(object):
+  def busyStudent(self, startTime, endTime, queryTime):
+    """
+    :type startTime: List[int]
+    :type endTime: List[int]
+    :type queryTime: int
+    :rtype: int
+    """
+    count = 0
+    for start, end in zip(startTime, endTime):
+      if start <= queryTime and queryTime <= end:
+        count += 1
+        
+    return count
