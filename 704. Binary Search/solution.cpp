fc92d96122b3fa20cae8e502dde4f19c16af16c4
ChibiKev <k3v.ch3n@gmail.com>
2022-01-20 12:12:10 -0500

    Added C++ Solution To Fourty Seventh Problem

---
+++ 704. Binary Search/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+  int search(vector<int>& nums, int target) {
+    int low = 0;
+    int high = nums.size() - 1;
+    while (low <= high){
+      int mid = low + ((high - low) / 2);
+      int value = nums[mid];
+      if (value == target){
+        return mid;
+      }
+      else if (value < target){
+        low = mid + 1;
+      }
+      else if (value > target){
+        high = mid - 1;
+      }
+    }
+    return -1;
+  }
+};
