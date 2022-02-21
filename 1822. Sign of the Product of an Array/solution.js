32fe684ff041895a178c8c905b853b1727bdabcd
ChibiKev <kevinchen.07cd@gmail.com>
2022-02-21 18:04:24 -0500

    Added Javascript Solution To Eighty Second Problem

---
+++ 1822. Sign of the Product of an Array/solution.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var arraySign = function(nums) {
+  let product = 1;
+  for (let num of nums){
+    if (num === 0){
+      return 0;
+    }
+    else if (num < 0){
+      product *= -1;
+    }
+  }
+  return product;
+};
