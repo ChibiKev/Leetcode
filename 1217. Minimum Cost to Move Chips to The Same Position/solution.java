80d5016bd1f2f3e7e45e58938b91488fa3c85c4e
ChibiKev <kevinchen.07cd@gmail.com>
2022-03-14 15:56:54 -0400

    Added Java Solution To One Hundred Third Problem

---
+++ 1217. Minimum Cost to Move Chips to The Same Position/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+  public int minCostToMoveChips(int[] position) {
+    int even = 0;
+    int odd = 0;
+    for (int index = 0; index < position.length; index++){
+      int chips = position[index];
+      if (chips % 2 == 0){
+        even += 1;
+      }
+      else{
+        odd += 1;
+      }
+    }
+
+    int result = even;
+    if (even > odd){
+      result = odd;
+    }
+
+    return result;
+  }
+}
