ed71ba65c8095660763e19cd87e37ecae591f360
ChibiKev <kevinchen.07cd@gmail.com>
2022-03-23 15:05:34 -0400

    Added C++ Solution To One Hundred Twelfth Problem

---
+++ 860. Lemonade Change/solution.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+  bool lemonadeChange(vector<int>& bills) {
+    int fives = 0;
+    int tens = 0;
+
+    for (int index = 0; index < bills.size(); index++){
+      int bill = bills[index];
+      if (bill == 5){
+        fives += 1;
+      }
+      else if (bill == 10){
+        fives -= 1;
+        tens += 1;
+      }
+      else if (bill == 20 && tens > 0){
+        fives -= 1;
+        tens -= 1;
+      }
+      else{
+        fives -= 3;
+      }
+      if (fives < 0 || tens < 0){
+        return false;
+      }
+    }
+
+    return true;
+  }
+};
