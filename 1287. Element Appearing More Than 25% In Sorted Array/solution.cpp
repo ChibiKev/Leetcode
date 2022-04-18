0e20a1ca68e0fd93572d8fe92e6dcbf316b21f43
ChibiKev <kevinchen.07cd@gmail.com>
2022-04-18 08:37:28 -0400

    Added C++ Solution To One Hundred Thirtieth Eighth Problem

---
+++ 1287. Element Appearing More Than 25% In Sorted Array/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+  int findSpecialInteger(vector<int>& arr) {
+    int arrLength = arr.size();
+    int split = arrLength / 4;
+
+    for (int index = 0; index < arrLength - split; index++){
+      int current = arr[index];
+      int end = arr[index + split];
+      if (current == end){
+        return current;
+      }
+    }
+
+    return -1;
+  }
+};
