c6acc8d5e5d2f79d38a61be6b4537784daf434b6
ChibiKev <kevinchen.07cd@gmail.com>
2022-04-23 09:56:57 -0400

    Added One Hundred Forty Third Problem

---
+++ 852. Peak Index in a Mountain Array/solution.py
@@ -0,0 +1,17 @@
+class Solution(object):
+  def peakIndexInMountainArray(self, arr):
+    """
+    :type arr: List[int]
+    :rtype: int
+    """
+    start = 0
+    end = len(arr) - 1
+    
+    while start < end:
+      mid = (start + end) / 2
+      if arr[mid] < arr[mid + 1]:
+        start = mid + 1
+      else:
+        end = mid
+    
+    return start
