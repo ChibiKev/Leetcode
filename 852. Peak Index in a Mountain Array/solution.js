5e36695939cf07e84fc489f091f87d73419feaf6
ChibiKev <kevinchen.07cd@gmail.com>
2022-04-23 09:57:10 -0400

    Added Javascript Solution To One Hundred Forty Third Problem

---
+++ 852. Peak Index in a Mountain Array/solution.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+ var peakIndexInMountainArray = function(arr) {
+  let start = 0;
+  let end = arr.length - 1;
+
+  while (start < end){
+    let mid = Math.floor((start + end) / 2);
+    if (arr[mid] < arr[mid + 1]){
+      start = mid + 1;
+    }
+    else{
+      end = mid;
+    }
+  }
+
+  return start;
+};
