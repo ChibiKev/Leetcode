a33a5ae2075887b79afd234628d36b08ac0b8c0e
ChibiKev <kevinchen.07cd@gmail.com>
2022-05-20 19:41:25 -0400

    Added One Hundred Seventieth Problem

---
+++ 1437. Check If All 1's Are at Least Length K Places Away/solution.py
@@ -0,0 +1,16 @@
+class Solution(object):
+  def kLengthApart(self, nums, k):
+    """
+    :type nums: List[int]
+    :type k: int
+    :rtype: bool
+    """
+    count = k
+    for num in nums:
+      if num == 1:
+        if count < k:
+          return False
+        count = 0
+      elif num == 0:
+        count += 1
+    return True
