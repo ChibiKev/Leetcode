0038ce8c154da05ea0fec6f903f294ca56c3cc2b
ChibiKev <25490322+ChibiKev@users.noreply.github.com>
2022-07-30 08:38:17 -0400

    Added Javascript Solution To Two Hundred Forty First Problem

---
+++ 412. Fizz Buzz/solution.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+ var fizzBuzz = function(n) {
+  let answer = [];
+  for (let index = 1; index < n + 1; index++){
+    if (index % 3 == 0 && index % 5 == 0){
+      answer.push("FizzBuzz");
+    }
+    else if (index % 3 == 0){
+      answer.push("Fizz");
+    }
+    else if (index % 5 == 0){
+      answer.push("Buzz");
+    }
+    else{
+      answer.push(index.toString());
+    }
+  }
+
+  return answer;
+};
