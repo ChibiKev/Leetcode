b949d77026c7c9f25ceb96459afd27b0830ba639
ChibiKev <25490322+ChibiKev@users.noreply.github.com>
2022-09-02 07:19:18 -0400

    Added C++ Solution To Two Hundred Seventy Fifth Problem

---
+++ 1189. Maximum Number of Balloons/solution.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+  int maxNumberOfBalloons(string text) {
+    int b = 0;
+    int a = 0;
+    int l = 0;
+    int o = 0;
+    int n = 0;
+
+    for (int index = 0; index < text.size(); index++){
+      char letter = text[index];
+      if (letter == 'b'){
+        b += 1;
+      }
+      else if (letter == 'a'){
+        a += 1;
+      }
+      else if (letter == 'l'){
+        l += 1;
+      }
+      else if (letter == 'o'){
+        o += 1;
+      }
+      else if (letter == 'n'){
+        n += 1;
+      }
+    }
+
+    int minimum = b;
+    if (a < minimum){
+      minimum = a;
+    }
+    if (l / 2 < minimum){
+     minimum = l / 2;   
+    }
+    if (o / 2 < minimum){
+      minimum = o / 2;    
+    }
+    if (n < minimum){
+      minimum = n;
+    }
+
+    return minimum;
+  }
+};
