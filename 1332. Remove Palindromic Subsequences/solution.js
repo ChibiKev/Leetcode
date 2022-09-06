1b3bb3e65385f2cdfa74b3d4187f224ed3cba2da
ChibiKev <25490322+ChibiKev@users.noreply.github.com>
2022-09-06 06:39:49 -0400

    Added Javascript Solution To Two Hundred Seventy Ninth Problem

---
+++ 1332. Remove Palindromic Subsequences/solution.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+ var removePalindromeSub = function(s) {
+  if (s == ''){
+    return 0;
+  }
+  else if (s == s.split("").reverse().join("")){
+    return 1;
+  }
+  return 2;
+};
