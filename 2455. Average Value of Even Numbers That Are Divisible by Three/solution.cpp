ac986a7848eb1064f5e7b7c6275af8bba81a8517
ChibiKev <25490322+ChibiKev@users.noreply.github.com>
2022-11-05 07:38:16 -0400

    Added C++ Solution for 2455

---
+++ 2455. Average Value of Even Numbers That Are Divisible by Three/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+  int averageValue(vector<int>& nums) {
+    int count = 0;
+    int total = 0;
+    for (int index = 0; index < nums.size(); index++){
+      int num = nums[index];
+      if (num % 2 == 0 && num % 3 == 0){
+        count += 1;
+        total += num;
+      }
+    }
+    
+    if (count == 0){
+      return 0;
+    }
+
+    return total / count;
+  }
+};
