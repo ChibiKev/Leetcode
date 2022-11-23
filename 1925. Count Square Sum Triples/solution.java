af53d5671ab7097d87e9b1d389ee073ac56910ac
ChibiKev <25490322+ChibiKev@users.noreply.github.com>
2022-11-23 08:06:53 -0500

    Added Java Solution for 1925

---
+++ 1925. Count Square Sum Triples/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+  public int countTriples(int n) {
+    int result = 0;
+
+    for (int a = 1; a < n; a++){
+      for (int b = a + 1; b < n; b++){
+        double c = Math.sqrt((a * a) + (b * b));
+        if ((int) c == c && c <= n){
+          result += 2;
+        }
+      }
+    }
+
+    return result;
+  }
+}
