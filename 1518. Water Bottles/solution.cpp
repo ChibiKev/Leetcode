5b84e5e8b5a3c02c622d7a2bc4cf1cef73dca80f
ChibiKev <25490322+ChibiKev@users.noreply.github.com>
2022-11-29 08:56:35 -0500

    Added C++ Solution for 1518

---
+++ 1518. Water Bottles/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+  int numWaterBottles(int numBottles, int numExchange) {
+    int result = numBottles + (numBottles / numExchange);
+    int count = (numBottles / numExchange) + (numBottles % numExchange);
+    
+    while (count >= numExchange){
+      result += (count / numExchange);
+      count = (count / numExchange) + (count % numExchange);
+    }
+
+    return result;
+  }
+};
