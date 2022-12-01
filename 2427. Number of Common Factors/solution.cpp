fdcf61d3df70fd3d282537d1fdef886e32e62fef
ChibiKev <25490322+ChibiKev@users.noreply.github.com>
2022-12-01 09:01:50 -0500

    Added C++ Solution for 2427

---
+++ 2427. Number of Common Factors/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+  int commonFactors(int a, int b) {
+    int count = 0;
+    int lowest = a;
+    if (lowest > b){
+      lowest = b;
+    }
+    
+    for (int index = 1; index < lowest + 1; index++){
+      if ((a % index == 0) && (b % index == 0)){
+        count += 1;
+      }
+    }
+    
+    return count;
+  }
+};
