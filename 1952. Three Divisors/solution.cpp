99b6fa2be839019bfdd709b2b6a6df861ae1185b
ChibiKev <25490322+ChibiKev@users.noreply.github.com>
2022-12-07 08:37:41 -0500

    Added C++ Solution for 1952

---
+++ 1952. Three Divisors/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+  bool isThree(int n) {
+    int count = 2;
+    for (int index = 2; index < n; index++){
+      if (n % index == 0){
+        count += 1;
+      }
+    }
+    return count == 3;
+  }
+};
