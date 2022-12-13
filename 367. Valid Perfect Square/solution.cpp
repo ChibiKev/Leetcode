72f2ec91f4cac6974ae90e987439e90a9a06ffb4
ChibiKev <25490322+ChibiKev@users.noreply.github.com>
2022-12-13 08:56:03 -0500

    Added C++ Solution for 367

---
+++ 367. Valid Perfect Square/solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+  bool isPerfectSquare(int num) {
+    if (num == 1){
+      return true;
+    }
+    
+    int left = 1;
+    int middle = num / 2;
+    int right = num;
+    
+    while (left <= right){
+      double squareroot = (double) num / (double) middle;
+      if (squareroot == middle){
+        return true;
+      }
+      else if (squareroot > middle){
+        left = middle + 1;
+      }
+      else if (squareroot < middle){
+        right = middle - 1;
+      }
+      middle = left + ((right - left) / 2);
+    }
+
+    return false;
+  }
+};
