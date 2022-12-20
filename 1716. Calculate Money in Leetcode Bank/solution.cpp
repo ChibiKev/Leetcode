9dd255d2980118c92105a6c5116d931ccecf69e1
ChibiKev <25490322+ChibiKev@users.noreply.github.com>
2022-12-20 08:57:48 -0500

    Added C++ Solution for 1716

---
+++ 1716. Calculate Money in Leetcode Bank/solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+  int totalMoney(int n) {
+    int completedWeeks = (n / 7);
+    int remainingDays = n % 7;
+    int completedWeekMoney = ((completedWeeks * (2 * 28 + (completedWeeks - 1) * 7)) / 2);
+    int remainingDaysMoney = ((remainingDays * (2 * (completedWeeks + 1) + (remainingDays - 1))) / 2);
+    return completedWeekMoney + remainingDaysMoney;
+  }
+};
