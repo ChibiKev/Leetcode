6c782ee37cf1ef2d531454a4af9fc2db07599aaa
ChibiKev <25490322+ChibiKev@users.noreply.github.com>
2023-01-08 07:44:41 -0500

    Added C++ Solution for 530

---
+++ 530. Minimum Absolute Difference in BST/solution.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+  int minimumDifference = INT_MAX;
+  int value = -1;
+  
+  void dfs(TreeNode* current){
+    if (current == NULL){
+      return;
+    }
+    dfs(current->left);
+    if (value != -1){
+      int difference = abs(current->val - value);
+      if (difference < minimumDifference){
+        minimumDifference = difference;
+      }
+    }
+    value = current->val;
+    dfs(current->right);
+  }
+public:
+  int getMinimumDifference(TreeNode* root) {
+    dfs(root);
+    return minimumDifference;
+  }
+};
