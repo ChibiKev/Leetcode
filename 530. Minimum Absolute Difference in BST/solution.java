35861027ae7164f0ec17e28da660e190353f06e6
ChibiKev <25490322+ChibiKev@users.noreply.github.com>
2023-01-08 07:49:49 -0500

    Added Java Solution for 530

---
+++ 530. Minimum Absolute Difference in BST/solution.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+  double minimumDifference = Double.POSITIVE_INFINITY;
+  int value = -1;
+  
+  void dfs(TreeNode current){
+    if (current == null){
+      return;
+    }
+    dfs(current.left);
+    if (value != -1){
+      int difference = Math.abs(current.val - value);
+      if (difference < minimumDifference){
+        minimumDifference = difference;
+      }
+    }
+    value = current.val;
+    dfs(current.right);
+  }
+  public int getMinimumDifference(TreeNode root) {
+    dfs(root);
+    return (int) minimumDifference; 
+  }
+}
