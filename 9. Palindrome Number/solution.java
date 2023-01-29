22e33c1a03d596dfafb2b4db8a12ebb1d70d0a7f
ChibiKev <25490322+ChibiKev@users.noreply.github.com>
2023-01-28 21:52:53 -0500

    Added Java Solution for 9

---
+++ 9. Palindrome Number/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+  public boolean isPalindrome(int x) {
+    if (x < 0){
+      return false;
+    }
+    
+    long reverseX = 0;
+    int xCopy = x;
+    while (xCopy > 0){
+      int lastDigit = xCopy % 10;
+      reverseX = reverseX * 10 + lastDigit;
+      xCopy = xCopy / 10;
+    }
+
+    return x == reverseX;
+  }
+}
