2181f0ae158c18371ef05d243718d7a0970c4cc0
ChibiKev <25490322+ChibiKev@users.noreply.github.com>
2023-02-22 08:16:30 -0500

    Added C++ Solution for 1486

---
+++ 1486. XOR Operation in an Array/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+  int xorOperation(int n, int start) {
+    int result = start;
+    for (int index = 1; index < n; index++){
+      int value = start + 2 * index;
+      result = result xor value;
+    }
+    return result;
+  }
+};
