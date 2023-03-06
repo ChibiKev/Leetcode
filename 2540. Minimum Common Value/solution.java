c9856ac7c13df4e5eef4f117574d9b3764cc51ec
ChibiKev <25490322+ChibiKev@users.noreply.github.com>
2023-03-06 08:46:40 -0500

    Added Java Solution for 2540

---
+++ 2540. Minimum Common Value/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+  public int getCommon(int[] nums1, int[] nums2) {
+    int nums1Index = 0;
+    int nums2Index = 0;
+    int result = -1;
+    while (nums1Index < nums1.length && nums2Index < nums2.length){
+      int nums1Value = nums1[nums1Index];
+      int nums2Value = nums2[nums2Index];
+      if (nums1Value == nums2Value){
+        result = nums1Value;
+        break;
+      }
+      else if (nums1Value > nums2Value){
+        nums2Index += 1;
+      }
+      else if (nums1Value < nums2Value){
+        nums1Index += 1;
+      }
+    }
+    return result;
+  }
+}
