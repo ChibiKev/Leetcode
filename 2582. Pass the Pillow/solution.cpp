eee35e626698abe5f0d446c577d9b507508e6744
ChibiKev <25490322+ChibiKev@users.noreply.github.com>
2023-03-17 07:39:24 -0400

    Added C++ Solution for 2582

---
+++ 2582. Pass the Pillow/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+  int passThePillow(int n, int time) {
+    int totalTimeOnePass = n - 1;
+    int totalPasses = time / totalTimeOnePass;
+    int personIndex = time % totalTimeOnePass;
+    if (totalPasses % 2 == 0){
+      return personIndex + 1;
+    }
+    return n - personIndex;
+  }
+};
