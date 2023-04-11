02fc9b22c396e71e49f852287fa23c775986c602
ChibiKev <25490322+ChibiKev@users.noreply.github.com>
2023-04-11 08:50:08 -0400

    Added C++ Solution for 1791

---
+++ 1791. Find Center of Star Graph/solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+  int findCenter(vector<vector<int>>& edges) {
+    int result = edges[0][1];
+    if (edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1]){
+      result = edges[0][0];
+    }
+    return result;
+  }
+};
