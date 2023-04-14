4daf419ae4717fef206279488ac709a4aa2fbf2e
ChibiKev <25490322+ChibiKev@users.noreply.github.com>
2023-04-14 08:29:51 -0400

    Added Java Solution for 2591

---
+++ 2591. Distribute Money to Maximum Children/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+  public int distMoney(int money, int children) {
+    int remainingMoney = money - children;
+    if (remainingMoney < 0){
+      return -1;
+    }
+    if (remainingMoney / 7 == children && remainingMoney % 7 == 0){
+      return children;
+    }
+    if (remainingMoney / 7 == children - 1 && remainingMoney % 7 == 3){
+      return children - 2;
+    }
+    
+    int result = children - 1;
+    if (remainingMoney / 7 < result){
+      result = remainingMoney / 7;
+    }
+    return result;
+  }
+}
