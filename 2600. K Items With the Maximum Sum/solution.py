27c6d58f6f00f57a95bb2220fc5a3d2eb714a2f0
ChibiKev <25490322+ChibiKev@users.noreply.github.com>
2023-04-16 08:17:27 -0400

    Added Problem and Python Solution for 2600

---
+++ 2600. K Items With the Maximum Sum/solution.py
@@ -0,0 +1,14 @@
+class Solution(object):
+  def kItemsWithMaximumSum(self, numOnes, numZeros, numNegOnes, k):
+    """
+    :type numOnes: int
+    :type numZeros: int
+    :type numNegOnes: int
+    :type k: int
+    :rtype: int
+    """
+    if k <= numOnes:
+      return k
+    if k <= numOnes + numZeros:
+      return numOnes
+    return numOnes - (k - numOnes - numZeros)
