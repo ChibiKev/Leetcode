d4060e633c0f82dd919022235d4961041fd8d4ff
ChibiKev <25490322+ChibiKev@users.noreply.github.com>
2023-08-04 07:18:20 -0400

    Added C++ Solution for 7

---
+++ 7. Reverse Integer/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+  int reverse(int x) {
+    long result = 0;
+
+    while (x != 0){
+      result = (result * 10) + (x % 10);
+      x /= 10;
+    }
+    
+    if (result < -pow(2, 31) || result > pow(2, 31) - 1){
+      return 0;
+    }
+    return int(result);
+  }
+};
